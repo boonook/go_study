7bcacfa10a13b84207057743703a27deec57d8e8
zhuxiuwu <1453855454@qq.com>
2019-12-26 14:53:27 +0800

    更新

--- response/response.go
+++ response/response.go
@@ -3,6 +3,10 @@ package response
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+
 	"github.com/huichen/pinyin"
 )
 
@@ -68,6 +72,10 @@ func Response() {
 	fmt.Println(py.GetPinyin('绿', true))
 	fmt.Println(py.GetPinyin('绿', false))
 	fmt.Println(py.GetPinyin('朱', false))
+	test, err := ReadAll("data/test.txt")
+	result := strings.Replace(string(test), "\n", "", 1)
+	fmt.Println("test_string", result)
+	WriteFile("data/test.txt")
 }
 
 ////数组去重
@@ -86,4 +94,31 @@ func RemoveRepeatedElement(arr []string) (newArr []string) {
 		}
 	}
 	return
+}
+
+/////读取文件内容
+func ReadAll(filePth string) ([]byte, error) {
+	f, err := os.Open(filePth)
+	if err != nil {
+		return nil, err
+	}
+	return ioutil.ReadAll(f)
+}
+
+/////将内容写入到文件中去
+func WriteFile(fileName string) {
+	f, err := os.OpenFile(fileName, os.O_WRONLY, 0644)
+	if err != nil {
+		// 打开文件失败处理
+
+	} else {
+		content := "写入的文件内容\n"
+
+		// 查找文件末尾的偏移量
+		n, _ := f.Seek(0, 2)
+
+		// 从末尾的偏移量开始写入内容
+		_, err = f.WriteAt([]byte(content), n)
+	}
+	defer f.Close()
 }
